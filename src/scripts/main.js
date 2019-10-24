a17c46e87eec68fe13470b2c88b239e76543810e
kostya-flern <kostya.reus64@gmail.com>
2019-10-24 14:22:20 +0300

    add carousel

---
+++ src/scripts/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/* конфигурация */
+const width = 900; // ширина картинки
+const count = 1; // видимое количество изображений
+let position = 0; // положение ленты прокрутки
+
+const list = document.querySelector('.carousel__container');
+const listElems = document.querySelectorAll('li');
+const leftMove = document.querySelector('.carousel__btn_prev');
+const rightMove = document.querySelector('.carousel__btn_next');
+
+leftMove.addEventListener('click', function() {
+  position += width * count;
+  position = Math.min(position, 0);
+  list.style.marginLeft = position + 'px';
+});
+
+rightMove.addEventListener('click', function() {
+  position -= width * count;
+  position = Math.max(position, -width * (listElems.length - count));
+  list.style.marginLeft = position + 'px';
+});
