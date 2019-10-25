5a61c78c93fe83b12191b1a3b86b0ece1675fdec
kostya-flern <kostya.reus64@gmail.com>
2019-10-25 15:05:55 +0300

    fix carousel

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,18 +6,23 @@ const count = 1; // видимое количество изображений
 let position = 0; // положение ленты прокрутки
 
 const list = document.querySelector('.carousel__container');
+let listElemsCount = list.childElementCount;
 const listElems = document.querySelectorAll('li');
 const leftMove = document.querySelector('.carousel__btn_prev');
 const rightMove = document.querySelector('.carousel__btn_next');
 
-leftMove.addEventListener('click', function() {
+leftMove.addEventListener('click', function () {
   position += width * count;
   position = Math.min(position, 0);
   list.style.marginLeft = position + 'px';
 });
 
-rightMove.addEventListener('click', function() {
-  position -= width * count;
-  position = Math.max(position, -width * (listElems.length - count));
-  list.style.marginLeft = position + 'px';
+rightMove.addEventListener('click', function () {
+  if (position > -width * (listElemsCount - 1)) {
+    position -= width * count;
+    position = Math.max(position, -width * (listElems.length - count));
+    list.style.marginLeft = position + 'px';
+  } else {
+    list.style.marginLeft = position + 'px';
+  }
 });
